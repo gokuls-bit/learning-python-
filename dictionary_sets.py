c63db560ba7915e353e82a4cfed67d4c92361d43
gokuls-bit <gokulsant684@gmail.com>
2025-05-24 19:02:41 +0530

    reinitializing

---
+++ dictionary_sets.py
@@ -0,0 +1,15 @@
+marks ={
+    "harry":45,
+    "mohit":46,
+    "vansh":96,
+    "karan":43,
+    "baljeet":72,
+    "shavy":63, 
+}
+#print (marks,type(marks))
+#print(marks.items())
+print(marks.keys())
+print(marks.values())
+marks.update({"bhuvar":69,"harry":99})
+print(marks)
+print(marks.get("shivika"))
