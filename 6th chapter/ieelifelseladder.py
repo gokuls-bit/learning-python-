0cd12f9836f9cf78d67892964062805cfae930dd
gokuls-bit <gokulsant684@gmail.com>
2025-05-28 00:00:03 +0530

    Save my changes before pulling

---
+++ 6th chapter/ieelifelseladder.py
@@ -0,0 +1,13 @@
+a = int(input("enter your age "))
+if(a>=18):
+    print("you are above age of concent")
+    print("good come-in")
+elif(a<0):
+    print("this is un-presented")
+elif(a==0): 
+    print("your age is not premerseble for this ")
+else:
+    print("you are below age ")
+    print("not allowed ")
+
+print("program ends ")
