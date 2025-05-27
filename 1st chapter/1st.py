10c6a398db720dc897518ff76cd19d10cf61def6
gokuls-bit <gokulsant684@gmail.com>
2025-05-28 00:05:13 +0530

    Save local changes before pull

--- 1st chapter/1st.py
+++
@@ -1,5 +0,0 @@
-#print("hello world ");
-#import pyjokes
-#joke = pyjokes.get_jokes()
-#print (joke)
-# repl :- read evaluave print loop
--- 3rd chapter/02_escape_sequence.py
+++
@@ -1,2 +0,0 @@
-a = "gokulkumarsant is now a part of brahmin sabah \n also he is \"cheif secretory of the youth cell \""
-print(a)
